4a6867c203a71a47085ee44a80388b6999858c27
aharsha7 <aharsha7na@gmail.com>
2025-04-16 16:24:27 +0530

    new

--- src/api/notesapi.js
+++ src/api/notesapi.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseURL = "https://notes-api-backend-iwuw.onrender.com/api/notes";
+// const baseURL = "https://notes-api-backend-rho.vercel.app/api/notes";
 // const baseURL = "http://localhost:5000/api/notes";
+
 const API = axios.create({
   baseURL: baseURL,
 });
 
-export const fetchNotes = () => API.get('/');
-export const createNote = (note) => API.post('/', note);
+export const fetchNotes = () => API.get("/");
+export const createNote = (note) => API.post("/", note);
 export const updateNote = (id, note) => API.put(`/${id}`, note);
 export const deleteNote = (id) => API.delete(`/${id}`);
